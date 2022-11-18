53e2df3983bb7b420d044f4bd68dd87f7dedb0dd
kianika <mona_yaghoubi@yahoo.com>
2022-11-18 22:36:00 +0100

    Med-14: Footer format is improved..

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,107 +5,75 @@ import { GrFacebookOption } from "react-icons/gr";
 import { AiOutlineInstagram } from "react-icons/ai";
 import { RiLinkedinFill } from "react-icons/ri";
 import { CgTwitter } from "react-icons/cg";
-
-const MainFooter = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 200px;
-  bottom: 0;
-`;
-const Container = styled.div``;
-const Col = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const Row = styled.div`
-  display: flex;
-  justify-content: space-around;
-`;
-const SocialIcons = styled.div`
-  display: flex;
-  justify-content: space-around;
-  padding: 30px 0;
-  font-size: 1.5rem;
-  color: #15a9e3;
-`;
-
-const Left = styled.div`
-  flex: 1;
-  display: flex;
-  align-items: center;
-`;
+import styles from "./Footer.module.scss";
 
 export default function Footer() {
   return (
-    <MainFooter>
-      <Container>
-        <Row>
-          <Col>
-            <Left>
-              <GiMedicines
-                size={25}
-                style={{ paddingRight: 8, color: "#15A9E3" }}
-              />
-              <h2 style={{ color: "#184363" }}>Happy Medi</h2>
-            </Left>
-            <SocialIcons>
-              <GrFacebookOption />
-              <AiOutlineInstagram />
-              <RiLinkedinFill />
-              <CgTwitter />
-            </SocialIcons>
-          </Col>
-          <Col>
-            <ul style={{ listStyleType: "none" }}>
-              <li style={{ padding: "10px 0px" }}>
-                <h4 style={{ color: "#15A9E3" }}>Need Help</h4>
-              </li>
-              <li style={{ padding: "10px 0px" }}>
-                <p>Monday - Friday: 9:00 - 20:00</p>
-              </li>
-              <li style={{ padding: "10px 0px" }}>
-                <p>Saturday: 9:00 - 15:00</p>
-              </li>
-              <li style={{ padding: "10px 0px" }}>Email: info@happymedi.at</li>
-            </ul>
-          </Col>
-          <Col>
-            <ul style={{ listStyleType: "none" }}>
-              <li style={{ padding: "10px 0px" }}>
-                <h4 style={{ color: "#15A9E3" }}>Information</h4>
-              </li>
-              <li style={{ padding: "10px 0px" }}>
-                <p>About Us</p>
-              </li>
-              <li style={{ padding: "10px 0px" }}>Delivery Information</li>
-              <li style={{ padding: "10px 0px" }}>
-                <p>Privacy and Policy</p>
-              </li>
-            </ul>
-          </Col>
-          <Col>
-            <ul style={{ listStyleType: "none" }}>
-              <li style={{ padding: "10px 0px" }}>
-                <h4 style={{ color: "#15A9E3" }}>Account</h4>
-              </li>
-              <li style={{ padding: "10px 0px" }}>
-                <p>Dashboard</p>
-              </li>
-              <li style={{ padding: "10px 0px" }}>
-                <p>My Orders</p>
-              </li>
-              <li style={{ padding: "10px 0px" }}>
-                <p>Account Details</p>
-              </li>
-            </ul>
-          </Col>
-        </Row>
-      </Container>
+    <div className={styles.container}>
+      <div className={styles.container__firstRow}>
+        <div className={styles.container__firstRow__first}>
+          <div className={styles.container__firstRow__first__logo}>
+            <GiMedicines
+              className={styles.container__firstRow__first__logo__icon}
+            />
+            <h2>Happy Medi</h2>
+          </div>
+          <div className={styles.container__firstRow__first__icons}>
+            <GrFacebookOption />
+            <AiOutlineInstagram />
+            <RiLinkedinFill />
+            <CgTwitter />
+          </div>
+        </div>
+        <div className={styles.container__firstRow__col}>
+          <ul>
+            <li>
+              <h4>Need Help</h4>
+            </li>
+            <li>
+              <p>Monday - Friday: 9:00 - 20:00</p>
+            </li>
+            <li>
+              <p>Saturday: 9:00 - 15:00</p>
+            </li>
+            <li>Email: info@happymedi.at</li>
+          </ul>
+        </div>
+        <div className={styles.container__firstRow__col}>
+          <ul>
+            <li>
+              <h4 >Information</h4>
+            </li>
+            <li>
+              <p>About Us</p>
+            </li>
+            <li>Delivery Information</li>
+            <li>
+              <p>Privacy and Policy</p>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.container__firstRow__col}>
+          <ul>
+            <li>
+              <h4>Account</h4>
+            </li>
+            <li>
+              <p>Dashboard</p>
+            </li>
+            <li>
+              <p>My Orders</p>
+            </li>
+            <li>
+              <p>Account Details</p>
+            </li>
+          </ul>
+        </div>
+      </div>
       <hr />
-      <Row>
+      <div className={styles.container__secondRow}>
         <p>&copy;{new Date().getFullYear()} Happy Medi | All Right Reserved</p>
-      </Row>
-    </MainFooter>
+      </div>
+    </div>
   );
 }
