da7c501efa3315b74ba0564828d872eac7ed2467
kianika <mona_yaghoubi@yahoo.com>
2022-11-29 09:23:09 +0100

    MED-15-body-slider is added to landing page.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Slider from "../../components/Slider/Slider";
+import styles from "./Home.module.scss"
 
 
 
 export default function Page() {
   return (
    
-   <div>
-
+   <div className={styles.Container}>
+      <Slider />
    </div>
     
   );
