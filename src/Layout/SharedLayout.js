a1fb69db8da6659df07aad1d506285535e681045
kianika <mona_yaghoubi@yahoo.com>
2022-12-23 14:56:13 +0100

    MED-18-LandingPage-Reforming: responsiveness done

--- src/Layout/SharedLayout.js
+++ src/Layout/SharedLayout.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
-import styled from "styled-components";
 import Footer from "../components/Footer/Footer";
 import Header from '../components/Header/Header';
 import NewsLetter from "../components/NewsLetter/NewsLetter";
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import { CiSearch } from "react-icons/ci";
 import { GiMedicines } from "react-icons/gi";
 import Form from "react-bootstrap/Form";
@@ -7,17 +8,30 @@ import styles from "./Header.module.scss";
 import { SlBasket } from "react-icons/sl";
 import { MdFavoriteBorder } from "react-icons/md";
 import { Link } from "react-router-dom";
-import Contac from "../../pages/Contact/Contact"
+import Contact from "../../pages/Contact/Contact";
+import { FiSearch } from "react-icons/fi";
+import { HiMenuAlt1 } from "react-icons/hi";
+import { IoCloseOutline } from "react-icons/io5";
 
 export default function Header() {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.container__topHeader}>
-      Due to the COVID 19 epidemic, orders may be processed with a slight delay
+        Due to the COVID 19 epidemic, orders may be processed with a slight
+        delay
       </div>
       <div className={styles.container__Header}>
         <div className={styles.container__Header__left}>
-          <GiMedicines size={35} style={{ color: "#15A9E3", paddingRight:"8px" }} />
+          <GiMedicines
+            size={35}
+            style={{ color: "#15A9E3", paddingRight: "8px" }}
+          />
           <h2>Happy Medi</h2>
         </div>
 
@@ -47,6 +61,14 @@ export default function Header() {
         <div className={styles.container__Header__right}>
           <AiOutlineUser /> My Account
         </div>
+        <div className={styles.container__Header__mobile}>
+          <div>
+            <FiSearch />
+          </div>
+          <div className={styles.container__Header__mobile__icon} onClick={handleClick}>
+           {clicked ? <IoCloseOutline/> : <HiMenuAlt1 />}
+          </div>
+        </div>
       </div>
 
       <hr />
@@ -71,11 +93,34 @@ export default function Header() {
           </nav>
         </div>
         <div className={styles.container__Navbar__right}>
-          <MdFavoriteBorder className={styles.container__Navbar__right__icon}/>
-          <SlBasket className={styles.container__Navbar__right__icon}/>
+          <MdFavoriteBorder className={styles.container__Navbar__right__icon} />
+          <SlBasket className={styles.container__Navbar__right__icon} />
         </div>
       </div>
+
+      <div className={clicked? styles.container__NavbarVisible : styles.container__NavbarHidden}>
+        <ul>
+          <li>
+          <Link to="/">Home</Link>
+          </li>
+          <li>
+          <Link to="">Shop by Brand</Link>
+          </li>
+          <li>
+          <Link to="">Shop by Category</Link>
+          </li>
+          <li>
+            <Link to="/Contact">Contact Us</Link>
+          </li>
+          <li>
+          <Link to="">All Categories</Link>
+          </li>
+          <li>
+          <Link to=""><AiOutlineUser /> My Account</Link> 
+          </li>
+        </ul>
+      </div>
       <hr />
-    </div>
+    </div> 
   );
 }
